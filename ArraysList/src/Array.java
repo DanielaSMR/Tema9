3813793f5c472253b2dd3326ae11fc6d88bbd030
DanielaSMR <10828775@ieselgrao.org>
2024-01-15 13:53:39 +0100

    g

---
+++ ArraysList/src/Array.java
@@ -0,0 +1,21 @@
+public class Array {
+
+        public Array(){
+
+        }
+
+        public leerValores(){
+            System.out.println("Dame numeros");
+
+        }
+
+        public calcularSuma(){
+
+        }
+
+        public mostrarResultados(){
+
+        }
+
+
+}
