0ed9a4e91a04944c8df53091c75ef025272aaa7d
DanielaSMR <10828775@ieselgrao.org>
2024-02-07 14:48:34 +0100

    g

--- Tienda/src/Lavadora.java
+++ Tienda/src/Lavadora.java
@@ -11,17 +11,8 @@ public class Lavadora extends Electrodomestico{
     }
 
 
-    
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-    
-      Lavadora nueva= new Lavadora (this.carga,);
-      return nueva;
-    }
-
-
-    public Lavadora(Lavadora copiaLav){
-        
+    public Lavadora(Lavadora copiaLav,String modelo, int precioBase, String consumoEnergetico, int peso){
+        super(modelo,precioBase,consumoEnergetico,peso);
         carga = copiaLav.carga;
         
 
